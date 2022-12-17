38d0920dcb851c9c5dfca2b019db3a06cebdd37a
HabKaffee <neknaum1@gmail.com>
2022-12-17 14:26:01 +0300

    sequential on cuda done

---
+++ main.cu
@@ -0,0 +1,156 @@
+#include<cuda.h>
+
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cinttypes>
+
+const double pi = std::atan(1) / 4;
+const double delta = 1e-4;
+const double eps = 1e-15;
+
+double Ax = -0.353, Bx = 0.353, Ay = 0.3, By = Ay, C = 3 * pi / 8;
+
+struct VarsToShare {
+  double Ax;
+  double Bx;
+  double Ay;
+  double By;
+  double C;
+  double Pi;
+  double Delta;
+  double Eps;
+} typedef Vars;
+
+__device__ double calculateDistance(double* x0, double* x1, size_t n) {
+  double sum = 0;
+  for (size_t i = 0; i < n; ++i) {
+    sum += std::pow(x0[i] - x1[i], 2);
+  }
+  return std::sqrt(sum);
+}
+
+__device__ void calculateF(double* result, double* input, Vars* vars, bool isSequential) {
+   if (isSequential) {
+    // printf("Sequential = true\n");
+    result[0] = input[0] + input[2] * std::cos(1.5 * vars->Pi - input[3]) - vars->Ax;
+    result[1] = input[1] + input[2] * std::cos(1.5 * vars->Pi + input[4]) - vars->Bx;
+    result[2] = input[2] + input[2] * std::sin(1.5 * vars->Pi - input[3]) - vars->Ay;
+    result[3] = (input[3] + input[4]) * input[2] + (input[1] - input[0]) - vars->C;
+    result[4] = input[2] + input[2] * std::sin(1.5 * vars->Pi + input[4]) - vars->By;
+  } else {
+    std::uint32_t id = threadIdx.x + blockIdx.x * blockDim.x;
+    // printf("Sequential = false\n");
+    // printf("id is %u\n", id);
+    switch(id) {
+      case 0:
+        printf("In 0\n");
+        result[0] = input[0] + input[2] * std::cos(1.5 * vars->Pi - input[3]) - vars->Ax;
+        break;
+      case 1:
+      printf("In 1\n");
+        result[1] = input[1] + input[2] * std::cos(1.5 * vars->Pi + input[4]) - vars->Bx;
+        break;
+      case 2:
+      printf("In 2\n");
+        result[2] = input[2] + input[2] * std::sin(1.5 * vars->Pi - input[3]) - vars->Ay;
+        break;
+      case 3:
+      printf("In 3\n");
+        result[3] = (input[3] + input[4]) * input[2] + (input[1] - input[0]) - vars->C;
+        break;
+      case 4:
+      printf("In 4\n");
+        result[4] = input[2] + input[2] * std::sin(1.5 * vars->Pi + input[4]) - vars->By;
+        break;
+      default:
+        printf("Wrong id!\n");
+    }
+  }
+}
+
+__device__ void print_progress(unsigned step, double* x0, double* x1) {
+  printf("-------------------\n");
+  printf("Progress\n");
+  printf("Step: %u\n", step);
+  printf("x1: %lf %lf\n", x0[0], x1[0]);
+  printf("x2: %lf %lf\n", x0[1], x1[1]);
+  printf("y: %lf %lf\n", x0[2], x1[2]);
+  printf("phi1: %lf %lf\n", x0[3], x1[3]);
+  printf("phi2: %lf %lf\n", x0[4], x1[4]);
+  printf("-------------------\n");
+}
+
+__global__ void calculateValue(double* x0, double* x1, Vars* vars, size_t n, bool isSequential) {
+  unsigned count = 0;
+  double* FValue = new double[n];
+  while(true) {
+    calculateF(FValue, x0, vars, isSequential);
+    for (size_t i = 0; i < n; ++i) {
+      x1[i] = x0[i] - FValue[i] * vars->Delta;
+    }
+    ++count;
+
+    if (!(count%5000)) print_progress(count, x0, x1);
+    
+    if (calculateDistance(x0, x1, n) < vars->Eps) break;
+    
+    for (size_t i = 0; i < n; ++i) {
+      x0[i] = x1[i];
+    }
+  }
+  delete[] FValue;
+}
+
+__host__ void print_result(double* x) {
+    printf("-------------------\n");
+    printf("Result\n");
+    printf("x1 : %lf\n", x[0]);
+    printf("x2 : %lf\n", x[1]);
+    printf("y : %lf\n", x[2]);
+    printf("phi1 : %lf\n", x[3]);
+    printf("phi2 : %lf\n", x[4]);
+    printf("-------------------\n");
+    printf("F(x) = {%.10e, %.10e, %.10e, %.10e, %.10e}\n",
+      x[0] + x[2] * std::cos(1.5 * pi - x[3]) - Ax,
+      x[1] + x[2] * std::cos(1.5 * pi + x[4]) - Bx,
+      x[2] + x[2] * std::sin(1.5 * pi - x[3]) - Ay,
+      (x[3] + x[4]) * x[2] + (x[1] - x[0]) - C,
+      x[2] + x[2] * std::sin(1.5 * pi + x[4]) - By
+    );
+}
+
+int main() {
+  Vars* vars;
+  const int NumOfEquations = 5;
+  double *x0, *x1;
+  cudaMallocManaged(&x0, sizeof(double) * NumOfEquations);
+  cudaMallocManaged(&x1, sizeof(double) * NumOfEquations);
+  //preassign vals
+  x0[0] = -0.1;
+  x0[1] = 0.1;
+  x0[2] = 0.0;
+  x0[3] = 2.0;
+  x0[4] = 2.0;
+
+  x1[0] = .0;
+  x1[1] = .0;
+  x1[2] = .0;
+  x1[3] = .0;
+  x1[4] = .0;
+  cudaMallocManaged(&vars, sizeof(Vars));
+  vars->Ax = Ax, vars->Ay = Ay, 
+  vars->Bx = Bx, vars->By = By, 
+  vars->C = C, vars->Pi = pi, 
+  vars->Delta = delta, vars->Eps = eps;
+
+  int numBlocks = 1, numThreadsPerBlock = 1;
+  bool isSequential = ((numBlocks == 1) && (numThreadsPerBlock == 1)) ? true : false;
+
+  calculateValue<<<numBlocks, numThreadsPerBlock>>>(x0, x1, vars, NumOfEquations, isSequential);
+  cudaDeviceSynchronize();
+  print_result(x0);
+  cudaFree(&x0);
+  cudaFree(&x1);
+  return 0;
+}
